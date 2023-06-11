a89e015bd1fb7ee6977544fa645451f97ca75a7e
Ritesh Verma <73749372+codeurritesh@users.noreply.github.com>
2023-06-11 23:50:39 +0530

    Update Count_Prefixes_of_a_Given_String.java

--- Count_Prefixes_of_a_Given_String.java
+++ Count_Prefixes_of_a_Given_String.java
@@ -3,6 +3,7 @@ public class Count_Prefixes_of_a_Given_String {
         String words[] = {"a","b","c","ab","bc","abc"}, s = "abc";
         System.out.println(countPrefixes(words,s));
     }
+//     good question
         public static int countPrefixes(String[] words, String s) {
             int count=0;
             for (String c:words) {
